f72011da7ab39d87eb92ff41a5fad032ff593674
li xiaobo <worldandyou@163.com>
2015-11-29 15:44:06 +0800

    add tooltiplabel component

--- tooltip/BubbleToolTipExample.java
+++ tooltip/BubbleToolTipExample.java
@@ -4,11 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.FlowLayout;
-import java.awt.Font;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import tooltip.BubbleToolTip.TooltipFont;
@@ -44,12 +41,7 @@ public class BubbleToolTipExample extends JFrame
 	private Component genSouthPanel()
 	{
 		String tooltipContent = "寥寥笔墨勾勒一青山\n青山下面一片大草原\n牵动着思绪纸上走牵动着思绪纸上走一遍一遍一遍\n借缕清风梦里好作帆";
-		BubbleToolTip tooltip = new BubbleToolTip(TooltipFont.YAHEI.getFont(), tooltipContent, this);
-		
-		JLabel helpLabelInfo = new JLabel(
-		        new ImageIcon(BubbleToolTip.class.getResource("help.png")));
-		helpLabelInfo.setText("Bubble Tool Tip");
-		helpLabelInfo.addMouseListener(tooltip);
+		TooltipLabel helpLabelInfo = new TooltipLabel(tooltipContent, TooltipFont.YAHEI.getFont(),Color.gray, Color.black, Color.orange, this);
 
 		JPanel panel = new JPanel(new FlowLayout());
 		panel.add(helpLabelInfo);
@@ -60,13 +52,7 @@ public class BubbleToolTipExample extends JFrame
 	private Component genNorthPanel()
 	{
 		String tooltipContent = "Dear lord when I get to heaven\nPlease let me bring my man\nWhen he comes tell me that you'll let me\nFather tell me if you can";
-		BubbleToolTip tooltip = new BubbleToolTip(tooltipContent, this);
-		
-		JLabel helpLabelInfo = new JLabel(
-		        new ImageIcon(BubbleToolTip.class.getResource("help.png")));
-		helpLabelInfo.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		helpLabelInfo.setText("Bubble Tool Tip");
-		helpLabelInfo.addMouseListener(tooltip);
+		TooltipLabel helpLabelInfo = new TooltipLabel(tooltipContent, null, this);
 		
 		JPanel panelNorth = new JPanel(new FlowLayout());
 		panelNorth.add(helpLabelInfo);
